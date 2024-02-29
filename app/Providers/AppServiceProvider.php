e9961ee962b39ad9e7ccf1c7444855b763e49011
Areg <aregghaz@gmail.com>
2024-02-29 19:58:55 +0400

    asd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(200);
+        Schema::defaultStringLength(191);
 
     }
 }
