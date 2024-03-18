ac4db47b2672347e31fb7fb2c030ad916b3fc2f3
Areg <aregghaz@gmail.com>
2024-03-18 16:32:59 +0400

    ad

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function address()
+    {
+        return $this->hasMany(Address::class, 'user_id', 'id');
+    }
 }
