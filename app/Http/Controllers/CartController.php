1e4c4779d3c0634921da0e86ef523febd7f532af
Areg <aregghaz@gmail.com>
2024-03-25 12:39:14 +0400

    asd

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -31,7 +31,7 @@ class CartController extends Controller
             'id' => $Product->id, // inique row ID
             'name' => $Product->name,
             'price' => $Product->price,
-            'image' => 'ssss',
+            'options' => ['image' => $Product->image],
             'quantity' => $qty,
         ));
         $cart = \Cart::getContent();
