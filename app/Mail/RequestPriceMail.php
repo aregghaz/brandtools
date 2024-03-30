a86044760cbde271975ced710464696b21faf4f8
Areg <aregghaz@gmail.com>
2024-03-30 06:13:06 +0400

    last_commit

--- app/Mail/RequestPriceMail.php
+++ app/Mail/RequestPriceMail.php
@@ -37,7 +37,7 @@ class RequestPriceMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.requestPrice',
         );
     }
 
