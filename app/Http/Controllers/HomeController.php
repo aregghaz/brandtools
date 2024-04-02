22b2a329a53f37c981467220557b5ff5ea1c9c28
Areg <aregghaz@gmail.com>
2024-04-02 10:55:09 +0400

    discount_pruice

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,6 +26,7 @@ class HomeController extends Controller
     public function singleProduct($slug): \Illuminate\Http\JsonResponse
     {
 
+        var_dump(is_string($slug),$slug);
         if(is_string($slug)){
             $product = Product::where('slug',$slug)->first();
 
